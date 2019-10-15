1e51cdf2bb75fb02325599ef6fbeafc7d428b3a5
Deshi Darveau-Willms <Deshi.DarveauWillms21@lethsd.ab.ca>
2019-10-15 14:50:02 -0600

    Bit reader outline

---
+++ src/main/java/frc/robot/util/BitReader.java
@@ -0,0 +1,16 @@
+package frc.robot.util;
+
+public class BitReader {
+	private int position = 0;
+
+	public BitReader() {}
+
+	public byte readByte() { return 0; }
+	public short readint16() { return 0; }
+	public int readint32() { return 0; }
+	public long readint64() { return 0; }
+	public short readuint16() { return 0; }
+	public int readuint32() { return 0; }
+	public long readuint64() { return 0; }
+	public String readString() { return ""; }
+}
