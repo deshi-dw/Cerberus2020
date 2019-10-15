0c3904facb546503b005403275741d51120d3aa2
deshi-dw <43869473+deshi-dw@users.noreply.github.com>
2019-10-15 05:55:55 -0600

    Command Trigger / Queue Added

--- src/main/java/frc/robot/commands/Command.java
+++ src/main/java/frc/robot/commands/Command.java
@@ -4,6 +4,8 @@ public interface Command extends Runnable {
 	public int getId();
 	public int getLength();
 
+	public boolean getIsFinished();
+
 	public void deserialize(byte[] data);
 	public byte[] serialize();
 }
---
+++ src/main/java/frc/robot/commands/CommandQueue.java
@@ -0,0 +1,53 @@
+package frc.robot.commands;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class CommandQueue implements Runnable {
+	private Queue<Command> stack = new ArrayBlockingQueue<Command>(16);
+	private Queue<Command> runnableStack;
+	private Command next;
+
+	private boolean isStopped = false;
+
+	public CommandQueue(Command[] commands) {
+		for (int i = 0; i < commands.length; i++) {
+			stack.add(commands[i]);
+		}
+	}
+
+	public void add(Command command) {
+		stack.add(command);
+	}
+
+	public void stop() {
+		isStopped = true;
+	}
+
+	public void reset() {
+		runnableStack = stack;
+		next = runnableStack.poll();
+	}
+
+	@Override
+	public void run() {
+		if (next == null) {
+			reset();
+		}
+
+		while (next != null) {
+			next.run();
+			while (next.getIsFinished() == false) {
+				if (isStopped) {
+					break;
+				}
+			}
+
+			if (isStopped) {
+				break;
+			}
+
+			next = runnableStack.poll();
+		}
+	}
+}
